3327b7d4ae1bba518e567f9126d0a1ad597aa9d6
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 18:34:24 +0200

    clean and change html stucture

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,37 +3,27 @@
 
 @section('content')
 
-    <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
-        <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
-            <a href="/">Home</a>
-            <a href="/products">Produits</a>
-            <a href="/categories">Categories</a>
-        </div>
-    </div>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
+            <div class="flex justify-center pt-12 sm:justify-start sm:pt-12">
+                @foreach($catalogues as $catalogue)
+                    <div class="catalogue">
+                        <div>
+                            <p>Catalogue {{ $catalogue->id }}</p>
+                        </div>
 
-    <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
-        <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
-            @foreach($catalogues as $catalogue)
-                <div class="catalogue">
-                    <div>
-                        <p>Catalogue {{ $catalogue->id }}</p>
+                        <div>
+                            <ul>
+                                @foreach($catalogue->produits as $produit)
+                                    <li class="product-container">
+                                        <product :produit="{{ $produit->toJson() }}"></product>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
-
-                    <div>
-                        <ul>
-                            @foreach($catalogue->produits as $produit)
-                                <li class="product-container">
-                                    <product :produit="{{ $produit->toJson() }}"></product>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
-            
+                @endforeach
+            </div>
         </div>
     </div>
 
