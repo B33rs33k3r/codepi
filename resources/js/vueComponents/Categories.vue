19ab8ef38a664f6d632b7544106927cce35e334d
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 18:03:39 +0200

    add categories vue component

---
+++ resources/js/vueComponents/Categories.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <div>
+            <p>Categories:</p>
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="categ in categs">{{ categ.name }} - {{ categ.id }}</li>
+            </ul>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+module.exports = {
+    mounted: function() { console.log(this.categories); },
+    props: ['categories'],
+    data: function() {
+        return {
+            categs: this.categories
+        };
+    }
+}
+</script>
