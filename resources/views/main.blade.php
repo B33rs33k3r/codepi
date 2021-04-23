84839ed0c60164d9143de1d3085e1346110b3fca
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 16:40:32 +0200

    raw display of data

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,8 +4,39 @@
 @section('content')
 
     <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
-        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-            <p>YOLO</p>
+        <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
+            @foreach($catalogues as $catalogue)
+                <div>
+                    <div>
+                        <p>Catalogue {{ $catalogue->id }}</p>
+                    </div>
+
+                    <div>
+                        <ul>
+                            @foreach($catalogue->produits as $produit)
+                                <li>
+                                    <div>
+                                        <p>Produit {{ $produit->id }}</p>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <p>1er caracteristique: {{ $produit->carac1 }}</p>
+                                        </div>
+                                        
+                                        <div>
+                                            <p>2eme caracteristique: {{ $produit->carac2 }}</p>
+                                        </div>
+
+                                        <div>
+                                            <p>3eme caracteristique: {{ $produit->carac3 }}</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 
