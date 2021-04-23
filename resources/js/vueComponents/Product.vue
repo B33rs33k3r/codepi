f9b02189a7ed8855020f505185aa17088035e10d
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 17:11:02 +0200

    create Product vue component

---
+++ resources/js/vueComponents/Product.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div class="product-container">
+        <div>
+            <p>Produit {{ product.id }}</p>
+        </div>
+
+        <div>
+            <div>
+                <p>1er Caracterisque: {{ product.carac1 }}</p>
+            </div>
+
+            <div>
+                <p>2eme Caracterisque: {{ product.carac2 }}</p>
+            </div>
+
+            <div>
+                <p>3eme Caracterisque: {{ product.carac3 }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    mounted: function() {},
+    props: ['produit'],
+    data: function() {
+        return {
+            product: this.produit
+        };
+    }
+}
+</script>
