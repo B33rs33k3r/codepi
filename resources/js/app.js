e7f2517262bc5a68b9fd73c6f2248e4e18313f21
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 16:38:47 +0200

    setup Vue instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+var vm = new Vue({
+    mounted: function() {
+        console.log('VueJS');
+    },
+    el: '#app',
+    data: function() {
+        return {};
+    }
+})
