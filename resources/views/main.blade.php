f5940cd70f6986a96cbee29a89f91dbb36dd17e2
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 15:57:13 +0200

    add main view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,12 @@
+
+@extends('welcome')
+
+@section('content')
+
+    <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
+        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            <p>YOLO</p>
+        </div>
+    </div>
+
+@endsection
