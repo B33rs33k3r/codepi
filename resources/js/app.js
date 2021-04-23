ae28aa2b80881821ed3915e062ba744612d88f5f
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 18:04:54 +0200

    call categories vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import Vue from 'vue';
 import Product from './/vueComponents/Product.vue';
 Vue.component('product', Product);
 
+import Categories from './/vueComponents/Categories.vue';
+Vue.component('categories', Categories);
+
 var vm = new Vue({
     el: '#app',
     data: function() {
