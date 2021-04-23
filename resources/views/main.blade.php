d063f2d3256d8575805a887df0e9dace8bd8aaed
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 18:17:20 +0200

    add home page link

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,14 @@
 
     <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
         <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
+            <a href="/">Home</a>
+            <a href="/products">Produits</a>
+            <a href="/categories">Categories</a>
+        </div>
+    </div>
+
+    <div class="max-w-12xl mx-auto sm:px-12 lg:px-12">
+        <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
             @foreach($catalogues as $catalogue)
                 <div class="catalogue">
                     <div>
