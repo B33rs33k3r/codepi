7f240236d2293b41e3a8363a1bddcb5838170309
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 20:22:26 +0200

    Use of vue-resource in Vue instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,19 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+import ProductContainer from './/vueComponents/ProductContainer.vue';
+Vue.component('product-container', ProductContainer);
 
 import Product from './/vueComponents/Product.vue';
 Vue.component('product', Product);
 
 import Categories from './/vueComponents/Categories.vue';
 Vue.component('categories', Categories);
+
 
 var vm = new Vue({
     el: '#app',
