c4bb5bf294a4ad8bf83f64f16faa098d8ec3f278
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-23 17:10:36 +0200

    add component in view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,22 +15,7 @@
                         <ul>
                             @foreach($catalogue->produits as $produit)
                                 <li>
-                                    <div>
-                                        <p>Produit {{ $produit->id }}</p>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <p>1er caracteristique: {{ $produit->carac1 }}</p>
-                                        </div>
-                                        
-                                        <div>
-                                            <p>2eme caracteristique: {{ $produit->carac2 }}</p>
-                                        </div>
-
-                                        <div>
-                                            <p>3eme caracteristique: {{ $produit->carac3 }}</p>
-                                        </div>
-                                    </div>
+                                    <product :produit="{{ $produit->toJson() }}"></product>
                                 </li>
                             @endforeach
                         </ul>
