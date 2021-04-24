41b87209c42989b566fe3fc10881d23cec46178d
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-24 09:59:59 +0200

    drop some unecessary dom

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -8,10 +8,6 @@
             <div>
                 <product-container :products="{{ $produits->toJson() }}"></product-container>
             </div>
-
-            <div class="flex justify-center pt-10 sm:justify-start sm:pt-10">
-                
-            </div>
         </div>
     </div>
 
