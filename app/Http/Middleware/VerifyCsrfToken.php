4263390e78c189e5cfb31d6c8678672d8c4a78cf
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-24 11:00:06 +0200

    exclude deleteproduct route from csrf middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/saveproduct',
-        '/savecategory'
+        '/savecategory',
+        '/deleteproduct'
     ];
 }
