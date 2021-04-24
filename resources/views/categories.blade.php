c7effa1de19d45effba1c343604f138cdf4f92f5
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-24 10:37:54 +0200

    display for categories page

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,4 +3,12 @@
 
 @section('content')
 
+<div class="container">
+    <div>
+        <div>
+            <categories-container :categories="{{ $categories->toJson() }}"></categories-container>
+        </div>
+    </div>
+</div>
+
 @endsection
