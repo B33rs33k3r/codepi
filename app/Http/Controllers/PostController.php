89584653c9bc8ac4f5825041608de95be9b284c0
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-24 09:53:56 +0200

    update and create action on PostController@product

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produits;
 
 class PostController extends Controller
 {
     public function product(Request $request)
     {
         $validated = $request->validate([
+            'id' => ['required', 'numeric'],
             'carac1' => ['required', 'string', 'max:510'],
             'carac2' => ['required', 'string', 'max:255'],
-            'carac3' => ['required', 'number', 'max:999']
+            'carac3' => ['required', 'string', 'max:999']
         ]);
 
-        return 'yolo';
+        $product = new Produits();
+
+        if ((int)$request->id > 0)
+            $product = Produits::find((int)$request->id);
+
+        $product->carac1 = $request->carac1;
+        $product->carac2 = $request->carac2;
+        $product->carac3 = $request->carac3;
+        $product->save();
+
+        return $product->toJson();
     }
 
     public function category(Request $request)
