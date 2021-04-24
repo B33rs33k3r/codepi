51e67212cf305b4ab43a0c48ff843b1595726b42
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-24 10:37:15 +0200

    categories vue component handle update query

--- resources/js/vueComponents/Categories.vue
+++ resources/js/vueComponents/Categories.vue
@@ -1,30 +1,31 @@
 
 <template>
     <div>
-        <div>
-            <p>Categories:</p>
-        </div>
+        <p>Categorie {{ categ.id }}</p>
+        <form @submit.prevent="save">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom:</label>
+                <input class="form-control" type="text" name="name" v-model="categ.name">
+            </div>
 
-        <div>
-            <ul>
-                <li v-for="categ in categs">
-                    <span class="bold">{{ categ.name }}</span>
-                </li>
-            </ul>
-        </div>
-
-
+            <button type="submit">Enregistrer</button>
+        </form>
     </div>
 </template>
 
 <script>
 module.exports = {
     mounted: function() {},
-    props: ['categories'],
+    props: ['category'],
     data: function() {
         return {
-            categs: this.categories
+            categ: this.category
         };
+    },
+    methods: {
+        save: function() {
+            this.$http.post('/savecategory', this.categ).then(response => {});
+        }
     }
 }
 </script>
