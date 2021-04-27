be26b59a46ac8152dd2c49ae71cb360163ca3fe1
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-27 09:25:31 +0200

    translation files

---
+++ resources/lang/en/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Home page translation file
+    |--------------------------------------------------------------------------
+    */
+
+    "title" => "codepi technical test",
+    "nav" => [
+        "catalog" => "catalog",
+        "product" => "product",
+        "category" => "category"
+    ]
+
+];
