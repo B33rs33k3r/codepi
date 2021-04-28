000e51b45e71e93f8ba0f227cf4bfaec917ba731
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 12:30:40 +0200

    add single product insertion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -48,7 +48,46 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'carac1' => ['required', 'string', 'max:510'],
+            'carac2' => ['required', 'string', 'max:255'],
+            'carac3' => ['required', 'numeric', 'max:999'],
+            'attach_catalogs' => ['array'],
+            'attach_catalogs.*' => ['numeric', 'min:1'],
+            'detach_catalogs' => ['array'],
+            'detach_catalogs.*' => ['numeric', 'min:1'],
+            'attach_categories' => ['array'],
+            'attach_categories.*' => ['numeric', 'min:1'],
+            'detach_categories' => ['array'],
+            'detach_categories.*' => ['numeric', 'min:1']
+        ]);
+
+        $product = new Product();
+        $product->carac1 = $request->carac1;
+        $product->carac2 = $request->carac2;
+        $product->carac3 = (int)$request->carac3;
+        $product->save();
+
+        if (!empty($request->attach_catalogs))
+            $product->catalogs()->attach($request->attach_catalogs);
+
+        if (!empty($request->detach_catalogs))
+            $product->catalogs()->detach($request->detach_catalogs);
+
+        if (!empty($request->attach_categories))
+            $product->categories()->attach($request->attach_categories);
+
+        if (!empty($request->detach_categories))
+            $product->categories()->detach($request->detach_categories);
+
+        $product->save();
+        $product->catalogs;
+        $product->categories;
+
+        return response()->json([
+            'message' => "Insertion complete successfully",
+            'product' => $product
+        ]);
     }
 
     /**
