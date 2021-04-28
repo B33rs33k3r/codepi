d9bf4bcc1a064239589530a7ded62535a537da29
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 05:21:42 +0200

    add components

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,19 @@ require('./bootstrap');
 
 import Vue from 'vue';
 
-import $ from 'jquery';
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 
-import Foundation from 'foundation-sites';
-Vue.use(Foundation);
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector("meta[name='csrf-token']").getAttribute('content');
+
+import ProductList from './/vueComponents/ProductList.vue';
+Vue.component('product-list', ProductList);
+
+import Product from './/vueComponents/Product.vue';
+Vue.component('product', Product);
+
+import ProductUpdate from './/vueComponents/ProductUpdate.vue';
+Vue.component('product-update', ProductUpdate);
 
 import ProductContainer from './/vueComponents/ProductContainer.vue';
 Vue.component('product-container', ProductContainer);
@@ -13,11 +22,9 @@ Vue.component('product-container', ProductContainer);
 import CategoriesContainer from './/vueComponents/CategoriesContainer.vue';
 Vue.component('categories-container', CategoriesContainer);
 
-import Product from './/vueComponents/Product.vue';
-Vue.component('product', Product);
 
-import Categories from './/vueComponents/Categories.vue';
-Vue.component('categories', Categories);
+import Category from './/vueComponents/Category.vue';
+Vue.component('category', Category);
 
 
 var vm = new Vue({
@@ -26,5 +33,3 @@ var vm = new Vue({
         return {};
     }
 });
-
-$(document).foundation();
