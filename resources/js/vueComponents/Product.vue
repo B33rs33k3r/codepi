4f36d88b4f5c0e2c7f295d8a2c87ed72ee0294b8
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 05:19:05 +0200

    handle single product display

--- resources/js/vueComponents/Product.vue
+++ resources/js/vueComponents/Product.vue
@@ -1,53 +1,116 @@
 
 <template>
-    <div class="product-container">
-        <div>
-            <p>Produit {{ product.id }}</p>
+    <div class="card product-card">
+        <div class="card-divider">
+            <div>
+                <p>Product {{ product.id }}</p>
+            </div>
+
+            <div class="product-action">
+                <ul>
+                    <li @click.prevent="switch_mode"><i class="fas fa-edit"></i></i></li>
+                </ul>
+            </div>
         </div>
 
-        <div>
-            <form @submit.prevent="save">
-                <div class="mb-3">
-                    <label for="carac1" class="form-label">1er Caracteristique:</label>
-                    <input class="form-control" type="text" name="carac1" v-model="product.carac1">
-                </div>
-                <div class="mb-3">
-                    <label for="carac2" class="form-label">2eme Caracteristique:</label>
-                    <input class="form-control" type="text" name="carac2" v-model="product.carac2">
-                </div>
-                <div>
-                    <label for="carac3" class="form-label">3eme Caracteristique:</label>
-                    <input class="form-control" type="number" name="carac3" v-model="product.carac3">
+        <div class="card-section">
+
+            <div v-show="!update_state">
+                <div class="category-linked category-block">
+                    <p>Linked Catalogs:</p>
+                    <ul class="grid-x grid-padding-x grid-margin-x category-detach" v-if="product.catalogs">
+                        <li class="cell auto category-item" v-for="catalog in product.catalogs">
+                            <p>{{ catalog.id }}</p>
+                        </li>
+                    </ul>
                 </div>
 
-                <button type="submit">Enregistrer</button>
-            </form>
-        </div>
+                <div class="product-caracs">
+                    <p>Carac produit:</p>
+                    <ul>
+                        <li class="product-carac"><p><span class="bold">Carac 1:</span> {{ product.carac1 }}</p></li>
+                        <li class="product-carac"><p><span class="bold">Carac 2:</span> {{ product.carac2 }}</p></li>
+                        <li class="product-carac"><p><span class="bold">Carac 3:</span> {{ product.carac3 }}</p></li>
+                    </ul>
+                </div>
 
-        <div>
-            <button @click.prevent="deletion">Supprimer</button>
+                <div class="category-linked category-block">
+                    <p>Linked Categories:</p>
+                    <ul class="grid-x grid-padding-x grid-margin-x category-detach" v-if="product.categories">
+                        <li class="cell auto category-item" v-for="category in product.categories">
+                            <p>{{ category.name }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="product-update" v-show="update_state">
+                <product-update
+                    :product_param="product"
+                    :category_param="categories"
+                    :catalog_param="catalogs"
+                    @delete="deletion">
+                </product-update>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 module.exports = {
-    mounted: function() {},
-    props: ['produit'],
+    mounted: function() { this.init(); },
+    props: ['catalog_param', 'product_param', 'category_param', ],
     data: function() {
         return {
-            product: this.produit
+            catalogs: this.catalog_param,
+            product: this.product_param,
+            categories: this.category_param,
+            update_state: false
         };
     },
+    watch: {
+        product_param: function(newvalue, oldvalue) {
+            this.product = newvalue;
+            this.switch_mode(false);
+        }
+    },
     methods: {
-        save: function() {
-            this.$http.post('/saveproduct', this.product).then(response => {
-                if (!response.ok)
-                    this.product = this.produit;
-            });
+        init: function() {
+            let product_catalogs = JSON.parse(JSON.stringify(this.product_param.catalogs));
+            this.filter_catalogs(product_catalogs);
+
+            let product_categories = JSON.parse(JSON.stringify(this.product_param.categories));
+            this.filter_categories(product_categories);
         },
-        deletion: function() {
-            this.$http.post('/deleteproduct', this.product).then(response => {})
+        deletion: function(id) {
+            this.$emit('delete', id);
+        },
+        filter_categories: function(categories) {
+            let shifted_category = categories.shift();
+
+            this.categories = this.categories.filter(function(category) {
+                return category.id != shifted_category.id;
+            });
+
+            if (categories.length > 0)
+                this.filter_categories(categories);
+
+            return;
+        },
+        filter_catalogs: function(catalogs) {
+            let shifted_catalog = catalogs.shift();
+
+            this.catalogs = this.catalogs.filter(function(catalog) {
+                return catalog.id != shifted_catalog.id
+            });
+
+            if (catalogs.length > 0)
+                this.filter_catalogs(catalogs);
+
+            return;
+        },
+        switch_mode: function(value) {
+            this.update_state = typeof value == 'boolean' ? value : !this.update_state;
         }
     }
 }
