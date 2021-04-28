e390524147eab258ed1ee785e610e8a4f208ffd5
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 05:18:39 +0200

    handle products display

---
+++ resources/js/vueComponents/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul class="grid-x grid-padding-x grid-padding-y">
+            <li v-for="product in products" class="cell small-12 medium-12 large-12">
+                <product
+                    :catalog_param="catalogs"
+                    :product_param="product"
+                    :category_param="categories"
+                    @delete="deletion">
+                </product>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    mounted: function() {},
+    props: ['catalog_list', 'product_list', 'category_list'],
+    data: function() {
+        return {
+            catalogs: this.catalog_list,
+            products: this.product_list,
+            categories: this.category_list
+        };
+    },
+    methods: {
+        deletion: function(id) {
+            this.products = this.products.filter(function(product) {
+                return product.id != id;
+            });
+        }
+    }
+}
+</script>
