ee50178bbf55ac001e7eea20dfe6bb9c9146e583
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 18:31:44 +0200

    add class

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    <div class="grid-x product-grid">
+    <div class="grid-x product-grid resource-grid">
         <div class="cell small-12 medium-10 large-8">
             <product-list
                 :catalog_list="{{ $catalogs->toJson() }}"
