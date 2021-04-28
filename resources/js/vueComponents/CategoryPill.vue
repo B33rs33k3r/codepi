6c8664b971088644702a4c6cdd8b172e444d00ff
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-28 18:30:55 +0200

    add single category pill component

---
+++ resources/js/vueComponents/CategoryPill.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card category-pill">
+        <p>{{ category.name }}</p>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    props: ['categoryItem'],
+    data: function() {
+        return {
+            category: this.categoryItem,
+        };
+    }
+}
+</script>
