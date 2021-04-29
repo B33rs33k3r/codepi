b8809ce8c27a615be48e220e5dcfe621076510bc
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-29 16:06:08 +0200

    update components import

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,42 +19,41 @@ Vue.component('message', Message);
 /**
  * Catalogs components.
  */
-import CatalogCreateForm from './/vueComponents/CatalogCreateForm.vue';
-Vue.component('catalog-create-form', CatalogCreateForm);
-
-import CatalogCard from './/vueComponents/CatalogCard.vue';
+import CatalogCard from './/vueComponents/Catalog/CatalogCard.vue';
 Vue.component('catalog-card', CatalogCard);
+
+import CatalogCreateForm from './/vueComponents/Catalog/Forms/CatalogCreateForm.vue';
+Vue.component('catalog-create-form', CatalogCreateForm);
 
 /**
  * Products components.
  */
-import ProductList from './/vueComponents/ProductList.vue';
+import ProductList from './/vueComponents/Product/ProductList.vue';
 Vue.component('product-list', ProductList);
 
-import Product from './/vueComponents/Product.vue';
+import Product from './/vueComponents/Product/Product.vue';
 Vue.component('product', Product);
 
-import ProductUpdate from './/vueComponents/ProductUpdate.vue';
+import ProductUpdate from './/vueComponents/Product/ProductUpdate.vue';
 Vue.component('product-update', ProductUpdate);
 
-import ProductCard from './/vueComponents/ProductCard.vue';
+import ProductCard from './/vueComponents/Product/ProductCard.vue';
 Vue.component('product-card', ProductCard);
 
 /**
  * Categories components.
  */
-import CategoryPill from './/vueComponents/CategoryPill.vue';
+import CategoryPill from './/vueComponents/Category/CategoryPill.vue';
 Vue.component('category-pill', CategoryPill);
 
-import CategoryCreateForm from './/vueComponents/CategoryCreateFrom.vue';
-Vue.component('category-create-form', CategoryCreateForm);
-
-import CategoryUpdateFrom from './/vueComponents/CategoryUpdateFrom';
-Vue.component('category-update-form', CategoryUpdateFrom);
-
-import CategoryCard from './/vueComponents/CategoryCard.vue';
+import CategoryCard from './/vueComponents/Category/CategoryCard.vue';
 Vue.component('category-card', CategoryCard);
 
+import CategoryCreateForm from './/vueComponents/Category/Forms/CategoryCreateFrom.vue';
+Vue.component('category-create-form', CategoryCreateForm);
+
+import CategoryUpdateFrom from './/vueComponents/Category/Forms/CategoryUpdateFrom';
+Vue.component('category-update-form', CategoryUpdateFrom);
 
 var vm = new Vue({
     el: '#app',
