28a9a556c2368ca413ae467d90825caace875310
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-29 15:14:30 +0200

    handle categories display

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,11 +3,33 @@
 
 @section('content')
 
-<div class="container">
-    <div>
-        <div>
-            <categories-container :categories="{{ $categories->toJson() }}"></categories-container>
-        </div>
+<div class="grid-x category-grid resource-grid">
+    <div class="cell small-12 medium-12 large-10">
+        <list :list="{{ $categories->toJson() }}">
+            <template #form>
+                <category-create-form></category-create-form>
+            </template>
+
+            <template #list-item="slotProps">
+                <category-card :category-item="slotProps.item">
+                    <template #list="slotProps">
+                        <list :list="slotProps.list" :list-class="slotProps.listClass">
+                            <template #list-item="slotProps">
+                                <product-card :product-item="slotProps.item">
+                                    <template #list="slotProps">
+                                        <list :list="slotProps.list" :list-class="slotProps.listClass">
+                                            <template #list-item="slotProps">
+                                                <category-pill :category-item="slotProps.item"></category-pill>
+                                            </template>
+                                        </list>
+                                    </template>
+                                </product-card>
+                            </template>
+                        </list>
+                    </template>
+                </category-card>
+            </template>
+        </list>
     </div>
 </div>
 
