fbb687c8a0d19fdc1a6db370bdc0ee6ceb4d60c1
Beerseeker <chirstophe.aupet@gmail.com>
2021-04-30 20:21:05 +0200

    drop unused file and rename products.blade to product.blade

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,14 +3,39 @@
 
 @section('content')
 
-    <div class="grid-x product-grid resource-grid">
-        <div class="cell small-12 medium-10 large-8">
-            <product-list
-                :catalog_list="{{ $catalogs->toJson() }}"
-                :product_list="{{ $products->toJson() }}"
-                :category_list="{{ $categories->toJson() }}">
-            </product-list>
-        </div>
+<div class="grid-x category-grid resource-grid">
+    <div class="cell small-12 medium-12 large-10">
+        <list :list="{{ $products->toJson() }}"
+            :optional-data="{catalogs: {{ $catalogs->toJson() }}, 'categories': {{ $categories->toJson() }}}">
+            <template #form="slotProps">
+                <product-create-form :catalog-list="slotProps.optionalData.catalogs"
+                    :category-list="slotProps.optionalData.categories">
+                </product-create-form>
+            </template>
+
+            <template #list-item="slotProps">
+                <product-card-action :product-item="slotProps.item"
+                    :catalog-list="slotProps.optionalData.catalogs"
+                    :category-list="slotProps.optionalData.categories">
+                    <template #list-catalog="slotProps">
+                        <list :list="slotProps.list" :list-class="slotProps.listClass">
+                            <template #list-item="slotProps">
+                                <catalog-pill :catalog-item="slotProps.item"></catalog-pill>
+                            </template>
+                        </list>
+                    </template>
+
+                    <template #list-category="slotProps">
+                        <list :list="slotProps.list" :list-class="slotProps.listClass">
+                            <template #list-item="slotProps">
+                                <category-pill :category-item="slotProps.item"></category-pill>
+                            </template>
+                        </list>
+                    </template>
+                </product-card-action>
+            </template>
+        </list>
     </div>
+</div>
 
 @endsection
